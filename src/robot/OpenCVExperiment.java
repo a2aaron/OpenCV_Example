9e74f05b7a6ea3eb9c2a2e17782d136b01f7ec29
Aaron Kofsky <a2aarontothe2@gmail.com>
2016-11-05 14:53:49 -0400

    Add some comments. Also add example.png to the gitignore
    This is so I don't forget why this works.

--- src/robot/OpenCVExperiment.java
+++ src/robot/OpenCVExperiment.java
@@ -14,27 +14,31 @@ import org.opencv.videoio.VideoCapture;
 
 public class OpenCVExperiment {
     public static void main(String[] args) {
+        // Load the OpenCV library
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        // Open the built in camera
         VideoCapture capture = new VideoCapture();
         capture.open(0);
-        Mat frame = new Mat();
+        // Wait a second for camera exposure
         try {
             Thread.sleep(1000);
         } catch (InterruptedException e1) {
             // TODO Auto-generated catch block
             e1.printStackTrace();
         }
+        // Read the image
+        Mat frame = new Mat();
         capture.read(frame);
+        
+        // Make the image greyscale
         Imgproc.cvtColor(frame, frame, Imgproc.COLOR_BGR2GRAY);
+        
+        // Read the image into a buffer that can be written (as .png)
         MatOfByte buffer = new MatOfByte();
-        Imgcodecs.imencode(".png", frame, buffer);
+        Imgcodecs.imencode(".jpg", frame, buffer);
         
-        
-        
-        
+        // Write the image to disk
         File image = new File("example.png");
-        
-        System.out.println(image.exists());
         FileOutputStream fos;
         try {
             fos = new FileOutputStream(image);
@@ -44,6 +48,7 @@ public class OpenCVExperiment {
             e.printStackTrace();
         }
         
+        System.out.println(image.exists());
         
         
     }
