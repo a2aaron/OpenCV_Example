0d458c9a045ffc5e044a9ff4d56e3273076c8fb7
Aaron Kofsky <a2aarontothe2@gmail.com>
2016-11-05 11:57:50 -0400

    Add OpenCV and ensure that it works with "Hello World"-style test

---
+++ src/robot/OpenCVExperiment.java
@@ -0,0 +1,13 @@
+package robot;
+
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+
+public class OpenCVExperiment {
+    public static void main(String[] args) {
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        Mat mat = Mat.eye(3, 3, CvType.CV_8UC1);
+        System.out.println(mat.dump());
+    }
+}
