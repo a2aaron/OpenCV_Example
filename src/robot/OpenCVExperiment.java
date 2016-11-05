addf9512e442088b3ff6279db8770dd06a63547f
Aaron Kofsky <a2aarontothe2@gmail.com>
2016-11-05 14:47:14 -0400

    Create Hello World Style Image Capture
    Program now captures an image via the camera and writes it to a file

--- src/robot/OpenCVExperiment.java
+++ src/robot/OpenCVExperiment.java
@@ -1,13 +1,50 @@
 package robot;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfByte;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.videoio.VideoCapture;
 
 public class OpenCVExperiment {
     public static void main(String[] args) {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-        Mat mat = Mat.eye(3, 3, CvType.CV_8UC1);
-        System.out.println(mat.dump());
+        VideoCapture capture = new VideoCapture();
+        capture.open(0);
+        Mat frame = new Mat();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
+        capture.read(frame);
+        Imgproc.cvtColor(frame, frame, Imgproc.COLOR_BGR2GRAY);
+        MatOfByte buffer = new MatOfByte();
+        Imgcodecs.imencode(".png", frame, buffer);
+        
+        
+        
+        
+        File image = new File("example.png");
+        
+        System.out.println(image.exists());
+        FileOutputStream fos;
+        try {
+            fos = new FileOutputStream(image);
+            fos.write(buffer.toArray());
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
+        
+        
     }
 }
